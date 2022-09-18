aa2d8acab3881e4ae1e1342144c7bdf4036db167
Kawin Viriyaprasopsook <kawin.vir@zercle.tech>
2022-09-18 12:43:34 +0700

    feat: try clean architecture

--- pkg/books/model.go
+++ pkg/books/model.go
@@ -6,15 +6,19 @@ import (
 	"gorm.io/gorm"
 )
 
+type BooksResponse struct {
+	Books *[]Book `json:"books"`
+}
+
 // Book Constructs your Book model under entities.
 type Book struct {
 	Id          uint           `json:"id" gorm:"primaryKey"`
 	Title       string         `json:"title" gorm:"size:127;index"`
 	Author      string         `json:"author" gorm:"size:127;index"`
 	Description string         `json:"description" gorm:""`
-	CreatedAt   *time.Time     `json:"createdAt" gorm:"autoCreateTime;index"`
-	UpdatedAt   *time.Time     `json:"updatedAt" gorm:"autoUpdateTime;index"`
-	DeletedAt   gorm.DeletedAt `json:"deletedAt" gorm:"index"`
+	CreatedAt   *time.Time     `json:"createdAt,omitempty" gorm:"autoCreateTime;index"`
+	UpdatedAt   *time.Time     `json:"updatedAt,omitempty" gorm:"autoUpdateTime;index"`
+	DeletedAt   gorm.DeletedAt `json:"deletedAt,omitempty" gorm:"index"`
 }
 
 type BookRepository interface {
