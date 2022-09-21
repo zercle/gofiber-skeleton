fd18cacd3138d0bc3f53ced1dda624584047ba66
Kawin Viriyaprasopsook <kawin.vir@zercle.tech>
2022-09-21 20:17:06 +0700

    feat: add user package

--- pkg/users/model.go
+++ pkg/users/model.go
@@ -11,9 +11,24 @@ type User struct {
 	Password    string         `json:"password" gorm:""`
 	RawPassword string         `json:"raw_password" gorm:"-"`
 	FullName    string         `json:"full_name" gorm:"size:127;index"`
-	CreatedAt   *time.Time      `json:"createdAt" gorm:"autoCreateTime;index"`
-	UpdatedAt   *time.Time      `json:"updatedAt" gorm:"autoUpdateTime;index"`
+	CreatedAt   *time.Time     `json:"createdAt" gorm:"autoCreateTime;index"`
+	UpdatedAt   *time.Time     `json:"updatedAt" gorm:"autoUpdateTime;index"`
 	DeletedAt   gorm.DeletedAt `json:"deletedAt" gorm:"index"`
 }
 
+type UserRepository interface {
+	GetUsers(fullname string) (*[]User, error)
+	GetUser(username string) (*User, error)
+	CreateUser(user *User) error
+	UpdateUser(username string, user *User) error
+	DeleteUser(username string) error
+}
 
+type UserService interface {
+	GetUsers(fullname string) (*[]User, error)
+	GetUser(username string) (*User, error)
+	CreateUser(user *User) error
+	UpdateUser(username string, user *User) error
+	DeleteUser(username string) error
+	ChangePassword(username string, oldPassword string, newPassword string, confirmPassword string) error
+}
