e6d15cc161749d1d8870c6e013f7fbac3deeb300
Kawin Viriyaprasopsook <kawin.vir@zercle.tech>
2022-09-24 11:11:57 +0700

    feat: separate interface from model

---
+++ pkg/users/interface.go
@@ -0,0 +1,18 @@
+package users
+
+type UserRepository interface {
+	GetUsers(fullname string) (*[]User, error)
+	GetUser(username string) (*User, error)
+	CreateUser(user *User) error
+	UpdateUser(username string, user *User) error
+	DeleteUser(username string) error
+}
+
+type UserService interface {
+	GetUsers(fullname string) (*[]User, error)
+	GetUser(username string) (*User, error)
+	CreateUser(user *User) error
+	UpdateUser(username string, user *User) error
+	DeleteUser(username string) error
+	ChangePassword(username string, oldPassword string, newPassword string, confirmPassword string) error
+}
