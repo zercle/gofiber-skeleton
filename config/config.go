e8602f394e0c3c3e1c64bb032cb5003b5f4ccfd3
Kawin Viriyaprasopsook <kawin.vir@zercle.tech>
2022-12-22 16:51:57 +0700

    ✏️config

--- config/config.go
+++ config/config.go
@@ -12,7 +12,7 @@ func LoadConfig(configName string) (err error) {
 	}
 	viper.SetConfigName(configName)  // name of config file (without extension)
 	viper.SetConfigType("yaml")      // REQUIRED if the config file does not have the extension in the name
-	viper.AddConfigPath("./configs") // path to look for the config file in
+	viper.AddConfigPath("./config") // path to look for the config file in
 	viper.AddConfigPath(".")         // optionally look for config in the working directory
 	err = viper.ReadInConfig()       // Find and read the config file
 	if err != nil {                  // Handle errors reading the config file
