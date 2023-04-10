0dde79a2cdb8850a575a0ad862b8ac2c17ec38ee
Kawin Viriyaprasopsook <kawin.v@kkumail.com>
2023-04-11 00:41:07 +0700

    🔧add env support

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	"github.com/zercle/gofiber-skelton/config"
+	"github.com/zercle/gofiber-skelton/pkg/config"
 	server "github.com/zercle/gofiber-skelton/internal/infrastructure"
 )
 
